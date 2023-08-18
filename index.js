4196bffead4297deedbb5648ebccb260a22fe1e2
Unique-utkarsh <131007191+Unique-utkarsh@users.noreply.github.com>
2023-08-18 14:13:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
-var testiomnialData = [
+var testimonialData = [
     {
         avatar: "https://img.freepik.com/free-photo/woman-with-long-hair-yellow-hoodie-with-word-music-it_1340-39068.jpg",
         name: "Utkarsh Dwivedi",
-        review: "Mind-blowing discovery! changed my routine. Essential for everyone. A vise advice to all interested. Can't imagine without it!"
+        review: "Mind-blowing discovery! changed my routine. Essential for everyone. A wise advice to all interested. Can't imagine without it!"
     },
+    // ... (other testimonial data entries)
     {
-        avatar: "https://img.freepik.com/free-photo/close-up-portrait-young-bearded-man-white-shirt-jacket-posing-camera-with-broad-smile-isolated-gray_171337-629.jpg",
-        name: "Alisha",
-        review: "Unbelievable gem! Altered my life. A must-have now. Wholeheartedly advise it to everyone. An absolute game-changer"
-    },
-    {
-        avatar: "https://img.freepik.com/free-photo/handsome-african-guy-with-stylish-haircut-taking-photo-digital-camera_171337-1345.jpg",
-        name: "Siddharth",
-        review: "Phenomenal addition! Completely transformed my days. Can't go without it. Strongly endorse for all. A game-changer for sure!"
-    },
-    {
-        avatar: "https://img.freepik.com/free-photo/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction_176420-15187.jpg",
-        name: "Madam President",
-        review: "Amazing product! It changed my life. Can't live without it now. Highly recommended to everyone!"
-    },
-    {
-        avatar: "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg",
-        name: "Rishi Sharma",
-        review: "Life-altering find! Indispensable now. Enthusiastically suggest to all. A game-changer for everyone!"
-    },
-    {
-        avatar: "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg",
-        name: "Aka",
-        review: "Life-altering find! Indispensable now. Enthusiastically suggest to all. A game-changer for everyone!"
-    }, {
         avatar: "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg",
         name: "Pankhuri",
         review: "Life-altering find! Indispensable now. Enthusiastically suggest to all. A game-changer for everyone!"
-    }]
-var slideHolder = document.querySelector("#slideHolder")
-for (let i of testiomnialData) slideHolder.innerHTML += <div class="swiper-slide"> <div class="ImgHolder"><img src="${i.avatar}"></div><div class="ContentHolder"><h3>${i.name}</h3><p>${i.review}</p></div></div>
+    }
+];
 
-const swiper = new Swiper('#craouselContainer', {
+var slideHolder = document.querySelector("#slideHolder");
+for (let i of testimonialData) {
+    slideHolder.innerHTML += `<div class="swiper-slide">
+        <div class="ImgHolder"><img src="${i.avatar}"></div>
+        <div class="ContentHolder"><h3>${i.name}</h3><p>${i.review}</p></div>
+    </div>`;
+}
+
+const swiper = new Swiper('#carouselContainer', {
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: 2.3,
@@ -54,12 +38,17 @@ const swiper = new Swiper('#craouselContainer', {
     },
     autoplay: { delay: 5000 }
 });
-window.onresize = queryResizer
-queryResizer()
+
+window.onresize = queryResizer;
+queryResizer();
+
 function queryResizer() {
-    if (window.innerWidth < 724) swiper.params.slidesPerView = 2
-    if (window.innerWidth > 501) swiper.params.slidesPerView = 2
-    if (window.innerWidth > 724) swiper.params.slidesPerView = 2.3
-    if (window.innerWidth < 501) swiper.params.slidesPerView = 1
-    swiper.update()
+    if (window.innerWidth < 501) {
+        swiper.params.slidesPerView = 1;
+    } else if (window.innerWidth < 724) {
+        swiper.params.slidesPerView = 2;
+    } else {
+        swiper.params.slidesPerView = 2.3;
+    }
+    swiper.update();
 }
